fdc3af1f3c4aae7a774a6d4a37db051bd69cb738
Shudong-Wang <907265139@qq.com>
2025-06-19 11:30:36 +0200

    add a simple DNN model for checking

---
+++ src/synapse/models/DNN.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+
+class DNN(nn.Module):
+    def __init__(self, input_dim, num_classes, hidden_dims):
+        super(DNN, self).__init__()
+
+        layers = []
+        
+        layers.append(nn.Linear(input_dim, hidden_dims[0]))
+        layers.append(nn.ReLU())
+        
+        for i in range(len(hidden_dims) - 1):
+            layers.append(nn.Linear(hidden_dims[i], hidden_dims[i+1]))
+            layers.append(nn.ReLU())
+        
+        layers.append(nn.Linear(hidden_dims[-1], num_classes))
+        
+        self.network = nn.Sequential(*layers)
+    
+    def forward(self, x):
+        return self.network(x)
